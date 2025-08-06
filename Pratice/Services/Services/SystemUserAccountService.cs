cfc62be981475bc9fe85486d608d97a43fe491e4
dothephi <phidtse161229@fpt.edu.vn>
2025-08-06 14:26:12 +0700

    "Update Serivce"

---
+++ Pratice/Services/Services/SystemUserAccountService.cs
@@ -0,0 +1,14 @@
+﻿using Services.Services.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Services
+{
+    public class SystemUserAccountService : ISystemUserAccountService
+    {
+
+    }
+}
