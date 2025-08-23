8eaec21350ed647276f8cb7e7b3360ed063f7788
dothephi <phidtse161229@fpt.edu.vn>
2025-08-24 01:22:06 +0700

    AddProductService

---
+++ Pratice/Services/Services/IServices/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using Model.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<ResponseDTO> CreateProductAsync(CreateAdminDTO createProductDTO);
+        Task<ResponseDTO> GetProductByIdAsync(int adminId);
+        Task<ResponseDTO> UpdateProductAsync(int adminId, UpdateAdminDTO updateAdminDTO);
+        Task<ResponseDTO> GetAllProductAsync();
+        Task<ResponseDTO> DeleteProductAsync();
+    }
+}
---
+++ Pratice/Services/Services/ProductService.cs
@@ -0,0 +1,14 @@
+﻿using BusinessLogicLayer.Services.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Services
+{
+    public class ProductService : IProductService
+    {
+
+    }
+}
